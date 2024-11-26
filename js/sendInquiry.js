d7e34b1f70cc6433b06618f8307a9af3c8c6cc8e
honey4nz <honey.malhotra2222@gmail.com>
2024-11-26 14:40:11 +1300

    initial commit

---
+++ js/sendInquiry.js
@@ -0,0 +1,9 @@
+//this function is to send inquiry
+
+const form = document.getElementsByTagName("form");
+const button = document.getElementsByClassName("theme-btn")[0];
+console.log(button);
+button.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //email code will go here
+});
